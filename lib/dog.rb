7ec6bea78032b61c89b7ed632ef1b0c3fa838000
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-14 15:27:49 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name=(dogs_name)
+    @name = dogs_name
+  end 
+  
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def name
+    @name 
+  end 
+end
