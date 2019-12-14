0854db821787862e5bbbf9b1f8a5a795af96219d
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-14 15:19:53 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end 
+  
+  def name 
+    @this_dogs_name
+  end
+  
+  def breed(dogs_breed)
+    @this_dogs_breed
+  end
+  def breed
+    @this_dogs_breed
+  end 
+end
+
+snoopy = Dog.new 
